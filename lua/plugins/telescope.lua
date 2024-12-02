7e69b15cb4d08958ba9ce697ca6dc52429cae2a1
machao <machao@qijiayoudao.com>
2024-12-02 11:01:14 +0800

    x

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -32,67 +32,55 @@ return {
     keys = function()
       local builtin = require("telescope.builtin");
       return {
-        {
-          "<leader>,",
-          "<cmd>Telescope buffers sort_mru=true sort_lastused=true<cr>",
-          desc = "切换 Buffer",
-        },
+        { "<leader><space>", require("telescope.builtin").find_files, desc = "搜索文件" },
+        { '<leader>"', builtin.registers, desc = "Registers" },
+        { "<leader>/", builtin.current_buffer_fuzzy_find, desc = "Buffer" },
         { "<leader>:", "<cmd>Telescope command_history<cr>", desc = "History Commands" },
-        { "<leader><space>", builtin.live_grep, desc = "Grep (Root Dir)" },
-        -- find
+        { "<leader>fs", builtin.live_grep, desc = "Grep (Root Dir)" },
         { "<leader>fb", "<cmd>Telescope buffers sort_mru=true sort_lastused=true<cr>", desc = "切换 Buffer" },
-        { "<leader>ff", require("telescope.builtin").find_files, desc = "搜索文件" },
-        -- { "<leader>fF", require("telescope.builtin").find_files({ root = false }), desc = "当前目录搜索文件" },
         { "<leader>fg", "<cmd>Telescope git_files<cr>", desc = "Find Files (git-files)" },
         { "<leader>fr", "<cmd>Telescope oldfiles<cr>", desc = "Recent" },
-        -- { "<leader>fR", require("telescope.builtin").oldfiles({ cwd = vim.uv.cwd() }), desc = "Recent (cwd)" },
-        -- git
-        { "<leader>gc", "<cmd>Telescope git_commits<CR>", desc = "Commits" },
-        -- { "<leader>gc", require("telescope.builtin").git_commits, desc = "Commits" },
-        { "<leader>gs", "<cmd>Telescope git_status<CR>", desc = "Status" },
+        { "<leader>gc", require("telescope.builtin").git_commits, desc = "Git Commits" },
+        { "<leader>gs", "<cmd>Telescope git_status<CR>", desc = "Git Status" },
         -- search
-        { '<leader>s"', "<cmd>Telescope registers<cr>", desc = "Registers" },
-        { "<leader>sa", "<cmd>Telescope autocommands<cr>", desc = "Auto Commands" },
-        { "<leader>sb", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Buffer" },
-        { "<leader>sc", "<cmd>Telescope command_history<cr>", desc = "Command History" },
-        { "<leader>sC", "<cmd>Telescope commands<cr>", desc = "Commands" },
-        { "<leader>sd", "<cmd>Telescope diagnostics bufnr=0<cr>", desc = "Document Diagnostics" },
-        { "<leader>sD", "<cmd>Telescope diagnostics<cr>", desc = "Workspace Diagnostics" },
-        { "<leader>sg", require("telescope.builtin").live_grep, desc = "Grep (Root Dir)" },
+        -- { "<leader>sa", "<cmd>Telescope autocommands<cr>", desc = "Auto Commands" },
+        -- { "<leader>sd", "<cmd>Telescope diagnostics bufnr=0<cr>", desc = "Document Diagnostics" },
+        -- { "<leader>sD", "<cmd>Telescope diagnostics<cr>", desc = "Workspace Diagnostics" },
+        -- { "<leader>sg", require("telescope.builtin").live_grep, desc = "Grep (Root Dir)" },
         -- { "<leader>sG", require("telescope.builtin").live_grep({ root = false }), desc = "Grep (cwd)" },
-        { "<leader>sh", "<cmd>Telescope help_tags<cr>", desc = "Help Pages" },
-        { "<leader>sH", "<cmd>Telescope highlights<cr>", desc = "Search Highlight Groups" },
-        { "<leader>sj", "<cmd>Telescope jumplist<cr>", desc = "Jumplist" },
-        { "<leader>sk", "<cmd>Telescope keymaps<cr>", desc = "Key Maps" },
-        { "<leader>sl", "<cmd>Telescope loclist<cr>", desc = "Location List" },
-        { "<leader>sM", "<cmd>Telescope man_pages<cr>", desc = "Man Pages" },
+        -- { "<leader>sh", "<cmd>Telescope help_tags<cr>", desc = "Help Pages" },
+        -- { "<leader>sH", "<cmd>Telescope highlights<cr>", desc = "Search Highlight Groups" },
+        -- { "<leader>sj", "<cmd>Telescope jumplist<cr>", desc = "Jumplist" },
+        -- { "<leader>sk", "<cmd>Telescope keymaps<cr>", desc = "Key Maps" },
+        -- { "<leader>sl", "<cmd>Telescope loclist<cr>", desc = "Location List" },
+        -- { "<leader>sM", "<cmd>Telescope man_pages<cr>", desc = "Man Pages" },
         { "<leader>sm", "<cmd>Telescope marks<cr>", desc = "Jump to Mark" },
-        { "<leader>so", "<cmd>Telescope vim_options<cr>", desc = "Options" },
-        { "<leader>sR", "<cmd>Telescope resume<cr>", desc = "Resume" },
+        {"<leader>so", "<cmd>Telescope vim_options<cr>", desc = "Options" },
+        -- { "<leader>sR", "<cmd>Telescope resume<cr>", desc = "Resume" },
         { "<leader>sq", "<cmd>Telescope quickfix<cr>", desc = "Quickfix List" },
         -- { "<leader>sw",  require("telescope.builtin").grep_string({ word_match = "-w" }), desc = "Word (Root Dir)" },
         -- { "<leader>sW",  require("telescope.builtin").grep_string({ root = false, word_match = "-w" }), desc = "Word (cwd)" },
         -- { "<leader>sw",  require("telescope.builtin").grep_string, mode = "v", desc = "Selection (Root Dir)" },
         -- { "<leader>sW",  require("telescope.builtin").grep_string({ root = false }), mode = "v", desc = "Selection (cwd)" },
         -- { "<leader>uC",  require("telescope.builtin").colorscheme({ enable_preview = true }), desc = "Colorscheme with Preview" },
-        {
-          "<leader>ss",
-          function()
-            require("telescope.builtin").lsp_document_symbols({
-              symbols = GlobalUtil.get_kind_filter(),
-            })
-          end,
-          desc = "Goto Symbol",
-        },
-        {
-          "<leader>sS",
-          function()
-            require("telescope.builtin").lsp_dynamic_workspace_symbols({
-              symbols = GlobalUtil.get_kind_filter(),
-            })
-          end,
-          desc = "Goto Symbol (Workspace)",
-        },
+        -- {
+        --   "<leader>ss",
+        --   function()
+        --     require("telescope.builtin").lsp_document_symbols({
+        --       symbols = GlobalUtil.get_kind_filter(),
+        --     })
+        --   end,
+        --   desc = "Goto Symbol",
+        -- },
+        -- {
+        --   "<leader>sS",
+        --   function()
+        --     require("telescope.builtin").lsp_dynamic_workspace_symbols({
+        --       symbols = GlobalUtil.get_kind_filter(),
+        --     })
+        --   end,
+        --   desc = "Goto Symbol (Workspace)",
+        -- },
       }
     end,
     opts = function()
@@ -116,8 +104,6 @@ return {
           end,
         })
       end
-
-      local actions = require("telescope.actions")
 
       local open_with_trouble = function(...)
         return require("trouble.sources.telescope").open(...)
