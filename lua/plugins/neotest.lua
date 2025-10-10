849fec76c2fcaba299a56b0f8b3e3eea5176a904
machao <machao@qijiayoudao.com>
2025-10-10 10:55:37 +0800

    master

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -142,7 +142,7 @@ return {
     { "<leader>tF", function() require("neotest").run.run(vim.uv.cwd()) end, desc = "Run All Test Files (Neotest)" },
     -- { "<leader>tl", function() require("neotest").run.run_last() end, desc = "Run Last (Neotest)" },
     { "<leader>ts", function() require("neotest").summary.toggle() end, desc = "Toggle Summary (Neotest)" },
-    { "<leader>to", function() require("neotest").output.open({ enter = true, auto_close = true }) end, desc = "Show Output (Neotest)" },
+    { "<leader>to", function() require("neotest").output.open({ short=false,enter = true, auto_close = true }) end, desc = "Show Output (Neotest)" },
     { "<leader>tO", function() require("neotest").output_panel.toggle() end, desc = "Toggle Output Panel (Neotest)" },
     { "<leader>tS", function() require("neotest").run.stop() end, desc = "Stop (Neotest)" },
     { "<leader>tw", function() require("neotest").watch.toggle(vim.fn.expand("%")) end, desc = "Toggle Watch (Neotest)" },
