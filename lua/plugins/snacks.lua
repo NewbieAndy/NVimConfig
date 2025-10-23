36be2549dcdc6d73a3c7fd7d795a6bc72237b1f0
Andy <machao.andy@gmail.com>
2025-10-23 15:34:45 +0800

    master

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -69,7 +69,7 @@ return {
 			statuscolumn = { enabled = true },
 			words = { enabled = true },
 			terminal = {
-				-- win = { position = "float" },
+				win = { position = "float" },
 			},
 			-- styles = {
 			-- 	notification = {
