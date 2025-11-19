261bafc8e51df147763d7d2e7b350f45144e3b57
Andy <machao.andy@gmail.com>
2025-11-19 14:29:45 +0800

    master

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,13 +3,9 @@ return {
 	lazy = false,
 	priority = 1000,
 	config = function()
-		-- require("tokyonight").setup({
-			-- on_highlights = function(hl, c)
-			-- 	-- hl.WinSeparator = { fg = "#ff0000", bg = "NONE" }
-			-- 	-- 调整补全弹窗背景和选中项，提升对比度
-			-- 	hl.Pmenu = { bg = "#22243b", fg = c.fg_dark }
-			-- end,
-		-- })
+		require("tokyonight").setup({
+			transparent = true
+		})
 		vim.cmd([[colorscheme tokyonight]])
 	end,
 }
