5509612e5d6c9a5532f7334a6653fdf6ae1dd000
Andy <machao.andy@gmail.com>
2025-11-20 20:01:04 +0800

    master

--- lua/plugins/kitty-scrollback.lua
+++ lua/plugins/kitty-scrollback.lua
@@ -1,16 +1,26 @@
 return {
-	"mikesmithgh/kitty-scrollback.nvim",
-	enabled = true,
-	lazy = true,
-	cmd = {
-		"KittyScrollbackGenerateKittens",
-		"KittyScrollbackCheckHealth",
-		"KittyScrollbackGenerateCommandLineEditing",
-	},
-	event = { "User KittyScrollbackLaunch" },
-	-- version = '*', -- latest stable version, may have breaking changes if major version changed
-	-- version = '^6.0.0', -- pin major version, include fixes and features that do not have breaking changes
-	config = function()
-		require("kitty-scrollback").setup()
-	end,
+  "mikesmithgh/kitty-scrollback.nvim",
+  enabled = true,
+  lazy = true,
+  cmd = {
+    "KittyScrollbackGenerateKittens",
+    "KittyScrollbackCheckHealth",
+    "KittyScrollbackGenerateCommandLineEditing",
+  },
+  event = { "User KittyScrollbackLaunch" },
+  -- version = '^6.0.0',
+  opts = {
+    default = {
+      keymaps_enabled = true,
+      restore_options = true,
+      status_window = { enabled = true, autoclose = true, show_timer = true },
+      paste_window = { yank_register = '"', yank_register_enabled = true },
+      kitty_get_text = { ansi = true, extent = 'all', clear_selection = true },
+    },
+    last_cmd = { kitty_get_text = { extent = 'last_cmd_output' } },
+    noansi = { kitty_get_text = { ansi = false, extent = 'all' } },
+  },
+  config = function(_, opts)
+    require('kitty-scrollback').setup(opts)
+  end,
 }
