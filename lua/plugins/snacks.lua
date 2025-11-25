cbac7476acb5a80617ca1bfabd5287cf9af6b123
Andy <machao.andy@gmail.com>
2025-11-25 19:10:43 +0800

    master

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -34,10 +34,9 @@ return {
 				win = {
 					input = {
 						keys = {
-							["<a-a>"] = {
-								"sidekick_send",
-								mode = { "n", "i" },
-							},
+							["<a-a>"] = { "sidekick_send", mode = { "n", "i" } },
+							["<c-b>"] = { "preview_scroll_up", mode = { "i", "n" } },
+							["<c-f>"] = { "preview_scroll_down", mode = { "i", "n" } },
 						},
 					},
 				},
@@ -202,6 +201,42 @@ return {
 				end,
 				desc = "Recent",
 			},
+			-- git
+			{
+				"<leader>gb",
+				function()
+					Snacks.picker.git_branches()
+				end,
+				desc = "Git Branches",
+			},
+			{
+				"<leader>gl",
+				function()
+					Snacks.picker.git_log()
+				end,
+				desc = "Git Log",
+			},
+			{
+				"<leader>gL",
+				function()
+					Snacks.picker.git_log_line()
+				end,
+				desc = "Git Log Line",
+			},
+			{
+				"<leader>gs",
+				function()
+					Snacks.picker.git_status()
+				end,
+				desc = "Git Status",
+			},
+			{
+				"<leader>gS",
+				function()
+					Snacks.picker.git_stash()
+				end,
+				desc = "Git Stash",
+			},
 			{
 				"<leader>gd",
 				function()
@@ -216,7 +251,6 @@ return {
 				end,
 				desc = "Git Log File",
 			},
-			-- Grep
 			-- search
 			{
 				'<leader>s"',
